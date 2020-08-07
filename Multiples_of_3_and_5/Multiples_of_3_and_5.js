c38b977b0ce6efe86b6ec41228ac9562312ed074
Tentakle__me <dima.zhurba.10@mail.ru>
2020-08-07 21:56:59 +0300

    commit project euler

---
+++ Multiples_of_3_and_5/Multiples_of_3_and_5.js
@@ -0,0 +1,8 @@
+	/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		Find the sum of all the multiples of 3 or 5 below 1000.*/
+
+function(multiple_1 multiple_2 natural) {
+	for(let i = 0; i <= natural; i++) {
+		
+	}
+}
